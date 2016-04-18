fd570f50e8e92662d0d2d30a568e018dc33307dd
Damien <damienhosenfeld@gmail.com>
2016-04-18 12:35:46 -0500

    Update Basic Math.py

--- Basic Math.py
+++ Basic Math.py
@@ -1,25 +1,32 @@
-Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:38:48) [MSC v.1900 32 bit (Intel)] on win32
+Python 3.5.1 (v3.5.1)
+Dec  6 2015, [MSC v.1900 32 bit (Intel)] on win32
 Type "copyright", "credits" or "license()" for more information.
+#Addition Operand
 >>> 2+2
 4
 >>> 6+5-3
 8
+#Subtraction Operand
 >>> 6-3
 3
 >>> 5-2-1
 2
+#Multipication Operand
 >>> 2*3
 6
 >>> 5*5
 25
+#Division Operand
 >>> 50/10
 5.0
 >>> 100/2
 50.0
+#Operational Order
 >>> 1+2-3*4/5
 0.6000000000000001
 >>> 5/4*3-2+1
 2.75
+#Parenthetical Order of Operations
 >>> 5/4*3-(2+1)
 0.75
 >>> -7
