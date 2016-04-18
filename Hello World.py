38c87b134dcd095e151d3e58056c1b4df64d4b31
Damien <damienhosenfeld@gmail.com>
2016-04-18 00:56:22 -0500

    Update Hello World.py

--- Hello World.py
+++ Hello World.py
@@ -2,6 +2,7 @@ Python 3.5.1 (v3.5.1)
 Dec  6 2015, [MSC v.1900 32 bit (Intel)] on win32
 Type "copyright", "credits" or "license()" for more information.
 
+#Prints "Hello World!"
 >>> print ('Hello World!')
 Hello World!
 >>> print ('Monty Python tribute!')
